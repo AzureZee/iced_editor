079d02fc55726ac57d2804058b9ed1cf1df88d4a
AzureZee <azureze@qq.com>
2025-12-23 00:50:27 +0800

    application

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::{io, path::Path, sync::Arc};
+
 use iced::{
-    widget::{container, text_editor},
-    Element, Sandbox, Settings,
+    executor,
+    widget::{column, container, horizontal_space, row, text, text_editor},
+    Application, Command, Element, Length, Settings, Theme,
 };
 
 fn main() -> iced::Result {
@@ -9,34 +12,64 @@ fn main() -> iced::Result {
 
 struct Editor {
     content: text_editor::Content,
+    error: Option<io::ErrorKind>,
 }
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
-impl Sandbox for Editor {
+impl Application for Editor {
     type Message = Message;
+    type Executor = executor::Default;
+    type Theme = Theme;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::new(),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                content: text_editor::Content::new(),
+                error: None,
+            },
+            Command::perform(
+                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
+                Message::FileOpened,
+            ),
+        )
     }
 
     fn title(&self) -> String {
         String::from("A cool editor!")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::Edit(action) => {
                 self.content.edit(action);
             }
+            Message::FileOpened(Ok(content)) => self.content = text_editor::Content::with(&content),
+            Message::FileOpened(Err(error)) => self.error = Some(error),
         }
+        Command::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
         let input = text_editor(&self.content).on_edit(Message::Edit);
-        container(input).padding(10).into()
+        let position = {
+            let (line, column) = self.content.cursor_position();
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+        let status_bar = row!(horizontal_space(Length::Fill), position);
+        container(column!(input, status_bar)).padding(10).into()
     }
+
+    fn theme(&self) -> Theme {
+        self::Theme::Dark
+    }
+}
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|err| err.kind())
 }
